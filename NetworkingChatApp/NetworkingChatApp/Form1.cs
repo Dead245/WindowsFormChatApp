b715fe12e074ccc363b5488b152e7194536637d7
Dead245 <116427900+Dead245@users.noreply.github.com>
2024-03-14 01:50:40 -0500

    form saved
    Moving the project to the repo folder apparently removed the forms for the wpf application?

---
+++ NetworkingChatApp/NetworkingChatApp/Form1.cs
@@ -0,0 +1,20 @@
+namespace NetworkingChatApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

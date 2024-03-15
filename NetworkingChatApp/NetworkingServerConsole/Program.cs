d50c09ff14c633c14e6f4447efbb463e3a452216
Dead245 <116427900+Dead245@users.noreply.github.com>
2024-03-15 01:47:59 -0500

    TCP server setup

--- NetworkingChatApp/NetworkingServerConsole/Program.cs
+++ NetworkingChatApp/NetworkingServerConsole/Program.cs
@@ -1,2 +1,27 @@
-﻿
-    Console.WriteLine("Nothing here yet!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Nothing done yet!");
+
+int port = 52100;
+var hostAddress = IPAddress.Parse("127.0.0.1");
+
+//Setup and start tcp server
+TcpListener tcpListener = new TcpListener(hostAddress, port);
+tcpListener.Start();
+
+//1024 bite size for incoming messages
+byte[] buffer = new byte[1024];
+string inputtedMessage;
+
+using TcpClient client = tcpListener.AcceptTcpClient();
+var tcpStream = client.GetStream();
+
+int readTotal;
+
+//Listen for client input
+while ((readTotal = tcpStream.Read(buffer, 0,buffer.Length)) != 0){
+    Console.WriteLine("Client Connected!");
+    inputtedMessage = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+}

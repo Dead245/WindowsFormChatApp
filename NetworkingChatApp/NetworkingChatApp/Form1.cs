16dce355e2d0fe9709037b8ad3cee4a1c4a9b829
Dead245 <116427900+Dead245@users.noreply.github.com>
2024-03-15 02:17:49 -0500

    made client disposable
    Used the "using" keyword for the server to not error out if the client closes their window and ends connection. Had to move location of TcpClient initialization to do so.

--- NetworkingChatApp/NetworkingChatApp/Form1.cs
+++ NetworkingChatApp/NetworkingChatApp/Form1.cs
@@ -5,7 +5,6 @@ namespace NetworkingChatApp
 {
     public partial class Form1 : Form
     {
-        TcpClient tcpClient = new TcpClient();
 
         int port = 52100;
         IPAddress hostAddress = IPAddress.Parse("127.0.0.1");
@@ -13,11 +12,14 @@ namespace NetworkingChatApp
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void ConnectButton_Click(object sender, EventArgs e)
         {
-            if(!tcpClient.Connected) tcpClient.Connect(hostAddress, port);
+            using TcpClient tcpClient = new TcpClient();
+
+            if (!tcpClient.Connected) tcpClient.Connect(hostAddress, port);
             ConnectButton.Text= "Connected!";
         }
     }
